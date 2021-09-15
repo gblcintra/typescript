6d018a53e74d88978ea96d73ac7068d6251540fc
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-15 11:19:17 -0300

    feat(any): type any(15092021)

---
+++ src/type_any.ts
@@ -0,0 +1,9 @@
+// "any" abre pra qualquer tipo de variavel (number, string,bolean ...)
+//por padrão a tipagem é entregue como "any"
+let precoProduto: any;
+
+precoProduto = true;
+precoProduto = 25.90;
+precoProduto = "25.90";
+
+console.log(precoProduto);
