28bc25e3416c48095cd58539075b0f5bb399930c
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-15 16:26:30 -0300

    feat(typla): type of tupla(15092021)

---
+++ src/type_tupla.ts
@@ -0,0 +1,14 @@
+//n'ao possue no JS e sim apenas com TS
+
+//receba apenas 2 parametros
+// O tipo '[string, number]'.A origem tem 2 elementos, o destino permite somente 2.
+let aluno: [string, number];
+
+aluno = ["Aluno 1", 123];
+aluno = ["Gabriel", 1]
+
+console.log("🚀 ~ file: type_tupla.ts ~ line 5 ~ aluno", aluno)
+
+let statusPedido: [string , string , string];
+
+statusPedido = ["Em produção", "Saiu para entrega", "Pedido entregue"]
