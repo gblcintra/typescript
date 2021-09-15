1567b9d3639115f8ad211024f474fc047bf08758
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-15 11:04:09 -0300

    feat(union): union type(15092021)

---
+++ src/union_type.ts
@@ -0,0 +1,16 @@
+//Atribuir para ser number ou string
+
+let userId1: number | string;
+let userId2: number | string;
+
+userId1 = 123;
+userId2 = "123";
+
+//Saber o typo da variavel
+console.log(typeof userId1);
+
+console.log(userId1);
+
+//Saber o typo da variavel
+console.log(typeof userId2);
+console.log(userId2);
