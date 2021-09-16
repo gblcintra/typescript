ff6490a64f45bec7a4ebe087a53c64fe17ea065c
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-16 15:06:04 -0300

    feat(arrowfunction): typing arrow function (16092021)

---
+++ src/Functions/arrowfunctions.ts
@@ -0,0 +1,13 @@
+//exemplo com arrow function
+
+
+
+// function retornoAPI(url: string):string{
+//     return url;
+// }
+
+const retornoAPI = (url: string): void => {
+    console.log("🚀 ~ file: arrowfunctions.ts ~ line 10 ~ retornoAPI ~ url", url)
+}
+
+retornoAPI('https://www.teste@teste.com/api');
