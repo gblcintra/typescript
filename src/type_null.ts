4923f133056a77a1b24c606531183eea5e725abf
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-16 09:26:32 -0300

    feat(null): type of null(16092021)

---
+++ src/type_null.ts
@@ -0,0 +1,19 @@
+//Type null
+//utiliza-se muito com union type
+//ex: let nome: string | null;
+//ou seja, ou ela é string ou é null
+//null é a ausencia de info
+let nme: null;
+
+nme = null
+console.log("🚀 ~ file: type_null.ts ~ line 9 ~ nme", nme)
+
+//null != undefined
+// não foi definida nada;
+let nomeUser;
+console.log("🚀 ~ file: type_null.ts ~ line 13 ~ nomeUser", nomeUser)
+
+
+
+
+
