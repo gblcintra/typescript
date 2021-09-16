7b7358334f235825d6d51a4cc9893a4bad3d8091
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-16 09:11:10 -0300

    feat(object): type of object(16092021)

--- src/type_object.ts
+++ src/type_object.ts
@@ -1,1 +1,12 @@
 //Type object
+
+let novoUsuario: object;
+
+novoUsuario = {
+    nome:"Gabriel",
+    email:"teste@teste.com",
+    idade: 26
+}
+
+console.log("🚀 ~ file: type_object.ts ~ line 6 ~ novoUsuario", novoUsuario);
+
